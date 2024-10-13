54ce2ffa1faac80cda1caa105a348d881fce7442
DanielBraunstain <161522700+DanielBraunstain@users.noreply.github.com>
2024-10-13 15:01:05 +0300

    mat.h

--- matrices_lib/mat.h
+++ matrices_lib/mat.h
@@ -2,43 +2,16 @@
 #define __MATRIX_H__
 
 #include <stddef.h>
-#define TOLERANCE 0.001F
 
+#define TOLERANCE 0.001F
 
 
 typedef struct matrix_t matrix_t;
 
 
-
-/** 
-*   MatrixCreate
-*   ------------
-*   Creates a new matrix according to the user specifications.
-*
-*   Params
-*   ------
-*   rows - number of rows in the matrix.
-*	cols - number of columns in the matrix.
-*	data - an array of the elements that will be inserted into the matrix.
-*   if data is NULL, a zero matrix will be created.
-*
-*   Return
-*   ------
-*   A pointer to the new matrix. NULL on failure.
-*/
 matrix_t* MatCreate(size_t n_rows, size_t n_cols, const float* data);
 
 
-/** 
-*   MatDestroy
-*   ----------
-*   Clears a matrix from memory.
-*   
-*   Params
-*   ------
-*   mat - a pointer to the matrix.
-*
-*/
 void MatDestroy(matrix_t* mat);
 
 /** 
@@ -50,7 +23,7 @@ void MatDestroy(matrix_t* mat);
 *   ------
 *   mat - a pointer to the matrix.
 *   row - the row which won't be included in the submatrix.
-*	cols - the column which won't be included in the submatrix.
+*	  col - the column which won't be included in the submatrix.
 *
 *   Return
 *   ------
@@ -72,34 +45,19 @@ matrix_t* MatSubmatrix(const matrix_t* mat, size_t row, size_t col);
 *   A pointer to the created identity matrix. NULL on failure.
 */
 matrix_t* MatI(size_t n);
-
 /** 
 *   MatAdd
 *   ------
-*   Adds two matrices.
-*
-*   Params
-*   ------
-*   mat1 - the 1st matrix of the calculation.
-*	mat2 - the 2nd matrix of the calculation.
-*
 *   Return
 *   ------
 *   A pointer to the result matrix.
-*   Returns NULL if the matrices dimensions don't match.
 */
+
 matrix_t* MatAdd(const matrix_t* mat1, const matrix_t* mat2);
 
 /** 
 *   MatMult
-*   -------
-*   Multiplies two matrices.
-*
-*   Params
 *   ------
-*   mat1 - the 1st matrix of the calculation.
-*	mat2 - the 2nd matrix of the calculation.
-*
 *   Return
 *   ------
 *   A pointer to the result matrix.
@@ -109,14 +67,7 @@ matrix_t* MatMult(const matrix_t* mat1, const matrix_t* mat2);
 
 /** 
 *   MatScalarMult
-*   -------------
-*   multiplies a matrix by scalar.
-*
-*   Params
 *   ------
-*   mat - a pointer to the matrix.
-*	scalar - the scalar.
-*
 *   Return
 *   ------
 *   A pointer to the newly created matrix that holds the result.
@@ -125,15 +76,8 @@ matrix_t* MatMult(const matrix_t* mat1, const matrix_t* mat2);
 matrix_t* MatScalarMult(matrix_t* mat, float scalar);
 
 /** 
-*   MatMult
-*   -------
-*   Compares mat1 to mat2.
-*
-*   Params
+*   MatCompare
 *   ------
-*   mat1 - the 1st matrix of the comparison
-*	mat1 - the 2nd matrix of the comparison
-*
 *   Return
 *   ------
 *   0 if the matrices are equal, nonzero otherwise.
@@ -142,13 +86,7 @@ int MatCompare(const matrix_t* mat1, const matrix_t* mat2);
 
 /**   
  *  MatTranspose
- *  ------------
- *  flips a matrix over its diagonal
- * 
- *  Params
- *  ------
- *  mat - a pointer to the matrix.
- * 
+ *  -------
  *  Return
  *  ------
  *  Transposed matrix. NULL on failure.
@@ -157,28 +95,16 @@ matrix_t* MatTranspose(const matrix_t* mat);
 
 /** 
 *   MatTrace
-*   -------
-*   Calculates the trace of the given matrix.
-*
-*   Params
 *   ------
-*   mat - a pointer to the matrix.
-*
 *   Return
 *   ------
-*   Trace of given matrix.
+*   Sum of diagnoal elements.
 */
 float MatTrace(const matrix_t* mat);
 
 /** 
 *   MatInvert
-*   ---------
-*   returns the inverse of a matrix.
-*
-*   Params
 *   ------
-*   mat - a pointer to the matrix.
-*
 *   Return
 *   ------
 *   pointer to inverted matrix. NULL on failure.
@@ -189,12 +115,6 @@ matrix_t* MatInvert(const matrix_t* mat);
 /** 
 *   MatDet
 *   ------
-*   calculates the determinant of given matrix.
-*
-*   Params
-*   ------
-*   mat - a pointer to the matrix.
-*
 *   Return
 *   ------
 *   float - value of matrices determinant.
@@ -204,28 +124,18 @@ float MatDet(const matrix_t* mat);
 
 /** 
 *   MatNorm
-*   --------
-*   calculates the frobinius norm (l2) of the matrix.
-*
-*   Params
 *   ------
-*   mat - a pointer to the matrix.
-* 
 *   Return
 *   ------
-*   The norm of the matrix.
+*   The  frobinius norm (l2) of the matrix.
 */
 float MatNorm(const matrix_t* mat);
 
 /** 
 *   MatShape
-*   --------
-*   Get the shape of a matrix.
-*
-*   Params
 *   ------
-*   mat - a pointer to the matrix.
-*   dims - a buffer where the result will be stored.
+*   Return
+*   ------
 *   dims[0] will be the number of rows.
 *   dims[1] will be the number of columns.
 *
@@ -234,18 +144,10 @@ void MatShape(const matrix_t* mat, size_t dims[2]);
 
 /** 
 *   MatGetElem
-*   ----------
-*   Get a value from a matrix.
-*
-*   Params
 *   ------
-*   mat - a pointer to the matrix.
-*   row - the value's row.
-*   col - the value's column.
-*
 *   Return
 *   ------
-*   The value at the intersection of the row and column.
+*   The value at the at mat[row][col].
 */
 float MatGetElem(const matrix_t* mat, size_t row, size_t col);
 
